1438ba38aeae2e8fd1d3b3bbc9b1e27771402965
SiggiS <siggis@gmail.com>
2022-12-11 15:14:56 +0100

    bauble added

--- App.tsx
+++ App.tsx
@@ -20,33 +20,97 @@ import {SafeAreaView, View, Button, ViewProps, StatusBar} from 'react-native';
 import {EngineView, useEngine} from '@babylonjs/react-native';
 import {SceneLoader} from '@babylonjs/core/Loading/sceneLoader';
 import {Camera} from '@babylonjs/core/Cameras/camera';
+import {Vector3,
+        Axis,
+        Space,
+        AbstractMesh,
+        Mesh,
+        HemisphericLight,
+        TransformNode,
+        FreeCamera} from '@babylonjs/core';
+    
+
 import {ArcRotateCamera} from '@babylonjs/core/Cameras/arcRotateCamera';
 import '@babylonjs/loaders/glTF';
 import {Scene} from '@babylonjs/core/scene';
-import {WebXRSessionManager, WebXRTrackingState} from '@babylonjs/core/XR';
+import { WebXRSessionManager, 
+         WebXRTrackingState, 
+         WebXRFeatureName, 
+         WebXRFeaturesManager,
+        } from '@babylonjs/core/XR';
 
+import { WebXRImageTracking } from '@babylonjs/core/XR/features/WebXRImageTracking';
+
+import { stat, DocumentDirectoryPath } from 'react-native-fs';
+import { trackerAssets } from '@assets/trackerassets';
+import { Dirs, FileSystem } from 'react-native-file-access';
+        
 const EngineScreen: FunctionComponent<ViewProps> = (props: ViewProps) => {
   const engine = useEngine();
   const [camera, setCamera] = useState<Camera>();
   const [xrSession, setXrSession] = useState<WebXRSessionManager>();
   const [trackingState, setTrackingState] = useState<WebXRTrackingState>();
   const [scene, setScene] = useState<Scene>();
+  const [imageTracking, setImageTracking] = useState<WebXRFeaturesManager>();
+  const [root, setRoot] = useState<TransformNode>();
+
+  const trackerimg = require('./assets/tracker5x5.png');
+  let item: AbstractMesh;
 
   const onToggleXr = useCallback(() => {
     (async () => {
       if (xrSession) {
         await xrSession.exitXRAsync();
+        if (root !== undefined)
+          root.setEnabled(false);
       } else {
         if (scene !== undefined) {
+
           const xr = await scene.createDefaultXRExperienceAsync({
             disableDefaultUI: true,
             disableTeleportation: true,
           });
-          const session = await xr.baseExperience.enterXRAsync(
-            'immersive-ar',
-            'unbounded',
-            xr.renderTarget,
-          );
+
+          const fm = xr.baseExperience.featuresManager;
+          const imgTracker = fm.enableFeature(WebXRFeatureName.IMAGE_TRACKING, 'latest', {
+            images: [
+                {
+                  src: 'https://siegfriedschaefer.github.io/rn-babylonjs-pg/assets/tracker5x5.png',
+                  estimatedRealWorldWidth: 1.0
+                  // uncomment for local ussage
+                  // src: 'http://192.168.178.26:8042/assets/tracker5x5.png',
+                },
+            ]
+        }) as WebXRImageTracking;
+
+        imgTracker.onTrackedImageUpdatedObservable.add((image) => {
+          if (root !== undefined) {       
+            root.setPreTransformMatrix(image.transformationMatrix);  
+            root.rotate(Axis.Y, 0.03, Space.LOCAL);
+
+            root.setEnabled(true);
+          }
+        })
+
+        imgTracker.onUntrackableImageFoundObservable.add((event) => {
+          console.log('untrackable' + event);
+        })
+
+        imgTracker.onTrackableImageFoundObservable.add((image) => {
+          console.log("onTrackableImageFoundObservable");
+        })
+
+        const session = await xr.baseExperience.enterXRAsync(
+          'immersive-ar',
+          'unbounded',
+          xr.renderTarget,
+        );
+
+        // const availableFeatures = fm.getEnabledFeatures();
+        // console.log(availableFeatures);
+
+          setImageTracking(fm);
+
           setXrSession(session);
           session.onXRSessionEnded.add(() => {
             setXrSession(undefined);
@@ -66,15 +130,60 @@ const EngineScreen: FunctionComponent<ViewProps> = (props: ViewProps) => {
 
   useEffect(() => {
     if (engine) {
-      const url =
-        'https://raw.githubusercontent.com/KhronosGroup/glTF-Sample-Models/master/2.0/BoxAnimated/glTF/BoxAnimated.gltf';
-      SceneLoader.LoadAsync(url, undefined, engine).then(loadScene => {
-        setScene(loadScene);
-        loadScene.createDefaultCameraOrLight(true, undefined, true);
-        (loadScene.activeCamera as ArcRotateCamera).alpha += Math.PI;
-        (loadScene.activeCamera as ArcRotateCamera).radius = 10;
-        setCamera(loadScene.activeCamera!);
-      });
+      var scene = new Scene(engine);
+      scene.createDefaultEnvironment({ createGround: false, createSkybox: false });
+      var camera = new FreeCamera("camera1", new Vector3(0, 5, -10), scene);
+      camera.setTarget(Vector3.Zero());
+
+      // this root node will be transformed by the image tracking module
+      const root = new TransformNode("root", scene);
+      root.setEnabled(false);
+      setRoot(root);
+      
+      // load the mesh
+      const loadmesh = async () => {
+ //       const model = await SceneLoader.ImportMeshAsync("", "https://assets.babylonjs.com/meshes/vintageDeskFan/", "vintageFan_animated.gltf", scene);
+        const model = await SceneLoader.ImportMeshAsync("", "https://siegfriedschaefer.github.io/rn-babylonjs-pg/assets/", "bauble_blue.glb", scene);
+        item = model.meshes[0];
+        item.scaling.scaleInPlace(1.0);
+        item.rotate(Axis.X, -Math.PI/2.0, Space.LOCAL);
+        item.translate(new Vector3(1, 0, 0), 2 );
+
+    
+        /*
+        // load animations from glTF
+        const fanRunning = scene.getAnimationGroupByName("fanRunning");
+    
+        // Stop all animations to make sure the asset it ready
+        scene.stopAllAnimations();
+        
+        // run the fanRunning animation
+        if (fanRunning !== null)
+          fanRunning.start(true);
+        */
+        item.parent = root;
+      };
+
+      loadmesh();
+
+      setScene(scene);
+      setCamera(scene.activeCamera!);
+
+      /*
+      var camera = new FreeCamera("camera1", new Vector3(0, 5, -10), scene);
+      camera.setTarget(Vector3.Zero());
+      let canvas: any = undefined;
+      camera.attachControl(canvas, true);
+      var light = new HemisphericLight("light1", new Vector3(0, 1, 0), scene);
+      light.intensity = 0.7;
+      var sphere = Mesh.CreateSphere("sphere1", 16, 2, scene);
+      sphere.position.y = 2;
+      sphere.position.z = 5;
+
+      setScene(scene);
+      setCamera(scene.activeCamera!);
+      */
+    
     }
   }, [engine]);
 
